0a090836144a03f86a2aa7fd89086135514a38f6
Abril Santiso Cabelluzi <abril.santiso@globant.com>
2022-02-24 18:25:55 -0300

    [ADD] Header styles

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -4,7 +4,13 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-    
+    <div className={styles.header_logoContainer}>
+      <Link to="/">KEDATAZO</Link>
+    </div>
+    <nav className={styles.header_nav}>
+      <Link to="/" className={`${styles.header_navItem} ${styles.header_linkHome}`}>Datazos</Link>
+      <Link to="/ke-es-esto" className={styles.header_navItem}>Ke es esto</Link>
+    </nav>
     </header>
   );
 }
