e0e1f34d48a8a82388ec41665c58dcc3af4aea44
Abril Santiso Cabelluzi <abril.santiso@globant.com>
2022-02-25 19:27:48 -0300

    [ADD] Basics animations

--- my-app/src/components/Card/Card.jsx
+++ my-app/src/components/Card/Card.jsx
@@ -3,16 +3,24 @@ import styles from "./Card.module.css";
 import datos from "../../data.json";
 
 export default function Card(props) {
-
-  const {funFactIndex, setNewFact} = props;
+  const { funFactIndex, setNewFact } = props;
 
   return (
     <div className={styles.card_cardContainer}>
       <div className={styles.card_contentContainer}>
-          <h4 className={styles.card_contentTitle}>{datos[funFactIndex].title}</h4>
-          <img src={datos[funFactIndex].img} alt={datos[funFactIndex].title} className={styles.card_Img}/>
-          <p dangerouslySetInnerHTML={{ __html: datos[funFactIndex].content }} className={styles.card_contentText}></p>
-          <RandomButton setNewFact={setNewFact}/>
+        <h4 className={styles.card_contentTitle}>
+          {datos[funFactIndex].title}
+        </h4>
+        <img
+          src={datos[funFactIndex].img}
+          alt={datos[funFactIndex].title}
+          className={styles.card_Img}
+        />
+        <p
+          dangerouslySetInnerHTML={{ __html: datos[funFactIndex].content }}
+          className={styles.card_contentText}
+        ></p>
+        <RandomButton setNewFact={setNewFact} />
       </div>
     </div>
   );
