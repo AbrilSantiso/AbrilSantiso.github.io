86e99987f7afc948e5d12962d935ac6c1e8921b6
Abril Santiso Cabelluzi <abril.santiso@globant.com>
2022-02-24 23:23:11 -0300

    Add styles

--- my-app/src/components/Footer/Footer.jsx
+++ my-app/src/components/Footer/Footer.jsx
@@ -4,7 +4,11 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-    
+     <div  className={styles.footer_contentContainer}>
+      <p>Copyright KEDATAZO - 2022 ©</p>
+      <div className={styles.footer_separationLine}></div>
+      <Link to="/mandanos-tu-dato" className={styles.footer_link}>Mandanos tu dato random</Link>
+     </div>
     </footer>
   );
 }
--- my-app/src/components/Home/Home.jsx
+++ my-app/src/components/Home/Home.jsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
 
 
 export default function Home() {
   return (
-    <div></div>
+    <div className={styles.home_container}>
+      <h1 className={styles.home_welcomeTitle}>¡Bienvenidx a KeDatazo!</h1>
+      <h2 className={styles.home_secondaryTitle}>Aca vas a encontrar miles de datos random muy loquitos que te van a gustar mucho bb! que disfrutes muchito xoxo</h2>
+      <div className={styles.card_cardContainer}>
+      </div>
+    </div>
   );
 }
